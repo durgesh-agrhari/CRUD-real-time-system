fbf4dc5bbe122427efc6df9a7da6af125594a56a
Durgesh <durgesh.1923it1156@kiet.edu>
2022-05-30 22:19:01 +0530

    Updates

---
+++ precache-manifest.b8cc31d54e9c5b5db446b286f52704a3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a1bfdc5c36702f7a1e38ee325d86a50",
+    "url": "/CRUD-real-time-system/index.html"
+  },
+  {
+    "revision": "cb0eecf8ed947923d210",
+    "url": "/CRUD-real-time-system/static/css/2.23fea534.chunk.css"
+  },
+  {
+    "revision": "e7a60361462662ebf3b9",
+    "url": "/CRUD-real-time-system/static/css/main.686842f3.chunk.css"
+  },
+  {
+    "revision": "cb0eecf8ed947923d210",
+    "url": "/CRUD-real-time-system/static/js/2.a683fee7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/CRUD-real-time-system/static/js/2.a683fee7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7a60361462662ebf3b9",
+    "url": "/CRUD-real-time-system/static/js/main.24ca4826.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/CRUD-real-time-system/static/js/main.24ca4826.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1c6822e495ad628542f",
+    "url": "/CRUD-real-time-system/static/js/runtime-main.9e167b54.js"
+  }
+]);
